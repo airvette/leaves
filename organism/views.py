8a5b8b25457b1140e824059914056391d7b35bb2
jguido <jguido@terrabella.com>
2017-12-28 09:50:34 -0800

    registered organism app and ran basic code

--- organism/views.py
+++ organism/views.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
 
 def index(request):
+    datetime_accessed = datetime.now()
+    date_accessed = datetime_accessed.date()
+    time_accessed = datetime_accessed.time()
+    date_format = '%a, %b %d, %Y'
+    time_format = '%I:%M %p'
     context = {
-        'message': 'This is a test post on the index page'
+        'message': 'Here is a test page.',
+        'date_accessed': date_accessed.strftime(date_format),
+        'time_accessed': time_accessed.strftime(time_format)
     }
     return render(request, 'organism/index.html', context)
